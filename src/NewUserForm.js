931597bb41224ddf7e75fda9c6e44cc68ed24ad5
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-11-07 17:23:28 -0500

    added form handling and state logic

--- src/NewUserForm.js
+++ src/NewUserForm.js
@@ -1,28 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 function NewUserForm () {
+    const INITIAL_STATE = { email: "", firstName: "", lastName: ""}
+
+    const [formData, setFormData] = useState(INITIAL_STATE)
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(formData => ({
+            ...formData,
+            [name]: value
+        }))
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setFormData(INITIAL_STATE)
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="email">Email</label>
             <input 
             type="text"
             name='email' 
             placeholder="email" 
-            value="email"
+            value={formData.email}
+            onChange={handleChange}
             />
             <label htmlFor="firstName">First Name</label>
             <input 
             type="text" 
             name='firstName'
             placeholder="First Name" 
-            value="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
             />
             <label htmlFor="lastName">Last Name</label>
             <input 
             type="text" 
             name='lastName'
             placeholder="Last Name" 
-            value="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
             />
             <button>Register!</button>
         </form>
