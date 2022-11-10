3f572bb1d1696bbf4aa81d96caa227aafb6adaf2
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-11-10 18:56:00 -0500

    added Not found page

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import {Route, Switch} from "react-router-dom"
 import UserRegisterForm from "./users/UserRegisterForm";
 import UserList from "./users/UsersList";
 import UserPage from './users/UserPage';
+import NotFound from './NotFound';
 
 function Routes() {
     return (
@@ -15,6 +16,9 @@ function Routes() {
             </Route>
             <Route exact path = "/admin">
                 <UserList/>
+            </Route>
+            <Route>
+                <NotFound/>
             </Route>
         </Switch>
     );
