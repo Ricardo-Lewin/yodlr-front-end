64cd1b8dd51fa6051abd01c10b4a52fe5fcde02d
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-11-16 20:06:43 -0500

    edited put route

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,42 +1,60 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3000";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
+
 
 class Api {
 
+    static async request(endpoint, paramsOrData = {}, verb = "get") {
+    
+  
+        console.debug("API Call:", endpoint, paramsOrData, verb);
+    
+        try {
+          return (await axios({
+            method: verb,
+            url: `${BASE_URL}/${endpoint}`,
+            [verb === "get" ? "params" : "data"]: paramsOrData})).data;
+            // axios sends query string data via the "params" key,
+            // and request body data via the "data" key,
+            // so the key we need depends on the HTTP verb
+        } catch(err) {
+          console.error("API Error:", err.response);
+          let message = err.response.data.message;
+          throw Array.isArray(message) ? message : [message];
+        }
+      }
 
     //API ROUTES
 
     //REGISTER
     static async register(data) {
-        const userData = {...data,['state']: 'active'}
-        const newUser = await axios.post(`${BASE_URL}/users`,userData)
-        console.log(newUser.data)
+        let res = await this.request(`users`, data, "post");
+        return res;
     }
 
     //GET ALL USERS - FOR ADMIN
     static async getAllUsers() {
-        const users = await axios.get(`${BASE_URL}/users`)
-        console.log(users)
-        console.log("call")
-        return users.data
+        let res = await this.request("users");
+        return res;
     }
 
     //GET USER
     static async getUser(id) {
-        const user = await axios.get(`${BASE_URL}/users/${id}`)
-        return user.data
+        let res = await this.request(`users/${id}`)
+        return res;
     }
 
     //PATCH USER
-    static async updateUser(user_id) {
-        let res = await this.request(`users/${user_id}`, "patch")
+    static async updateUser(id,data) {
+
+        let res = await this.request(`users/${id}`, data, "put")
         return res;
     }
 
     //DELETE USER
-    static async deleteUser(user_id) {
-        let res = await this.request(`users/${user_id}`, "delete")
+    static async deleteUser(id) {
+        let res = await this.request(`users/${id}`, "delete")
         return res;
     }
 
