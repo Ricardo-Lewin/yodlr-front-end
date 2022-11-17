9afc6f959ba09a49217a8a47b996e494c314d1d8
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-11-16 20:08:53 -0500

    removed bullets from list view

--- src/users/UserPage.js
+++ src/users/UserPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import Api from "../api/Api";
 import UserCard from "./UserCard"
+import "./UserPage.css"
 
 function UserPage() {
     const { id } = useParams();
@@ -19,12 +20,19 @@ function UserPage() {
         fetchUser();
     }, [id]);
 
+    const updateStatus= async (data) => {
+        
+        const user = await Api.updateUser(id, data)
+        setUser(user)
+        console.log(user)
+    }
+
     if (!user) return <p>Loading...</p>
 
     return(
         <div>
             <h2>User Profile</h2>
-            <UserCard key={user.id} id={user.id} email={user.email} firstName={user.firstName} lastName={user.lastName} state={user.state} />
+            <UserCard key={user.id} id={user.id} email={user.email} firstName={user.firstName} lastName={user.lastName} state={user.state} updateStatus={updateStatus}/>
         </div>
     )
 
