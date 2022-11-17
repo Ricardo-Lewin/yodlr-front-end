dabb254fbede978d0baa5c1d8cb2c8193177bbb5
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-11-16 20:08:02 -0500

    added state toggling and view logic

--- src/users/UserCard.js
+++ src/users/UserCard.js
@@ -1,19 +1,34 @@
 import React from "react"
 import {Link} from "react-router-dom"
 import Card from "react-bootstrap/Card";
+import { Button } from "react-bootstrap";
 
-function UserCard({ id, email, firstName, lastName, state }) {
+
+function UserCard({ id, email, firstName, lastName, state, updateStatus, showLink = false, showButton = false}) {
+    
+    const onClick = () => {
+        const data = {
+            id,
+            firstName,
+            lastName,
+            email,
+            state: (state === "pending") ? "active" : "pending"
+        }
+        updateStatus(data)
+    }
     return(
         <li>
             <Card style={{width: '18rem'}}>
                 <Card.Body>
                     <Card.Title>{lastName}, {firstName}</Card.Title>
-                    <Card.Text>
-                        <div>User ID: {id}</div>
-                        <div>Email: {email}</div>
-                        <div>State: {state}</div>
-                        <div><Link to={`users/${id}`}>Edit Profile</Link></div>
-                    </Card.Text>
+                    <div>
+                        <Card.Text>User ID: {id}</Card.Text>
+                        <Card.Text>Email: {email}</Card.Text>
+                        <Card.Text>State: {state}</Card.Text>
+                        {!showButton && <Button size="sm" onClick={onClick}>Toggle Status</Button>}
+                        
+                        {showLink && <Link to={`users/${id}`}>Edit Profile</Link>}
+                    </div>
                 </Card.Body>
             </Card>
         </li>
